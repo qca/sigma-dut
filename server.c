qca/sigma-dut

commit 9a742ff95185beb2fe53bd75b2c93ac35cb16399

Add Program,HS2-R4 value
Signed-off-by: Jouni Malinen <quic_jouni@quicinc.com>

server.c
@@ -321,7 +321,8 @@ static enum sigma_cmd_result cmd_server_reset_default(struct sigma_dut *dut,
 	}
 
 	prog = sigma_program_to_enum(var);
-	if (prog != PROGRAM_HS2_R2 && prog != PROGRAM_HS2_R3) {
+	if (prog != PROGRAM_HS2_R2 && prog != PROGRAM_HS2_R3 &&
+	    prog != PROGRAM_HS2_R4) {
 		send_resp(dut, conn, SIGMA_ERROR,
 			  "errorCode,Unsupported program");
 		return STATUS_SENT;
@@ -877,8 +878,8 @@ static enum sigma_cmd_result cmd_server_request_status(struct sigma_dut *dut,
 	}
 
 	prog = sigma_program_to_enum(var);
-	if (prog != PROGRAM_HS2_R2 && prog != PROGRAM_HS2_R3) {
-		send_resp(dut, conn, SIGMA_ERROR,
+	if (prog != PROGRAM_HS2_R2 && prog != PROGRAM_HS2_R3 &&
+	    prog != PROGRAM_HS2_R4) {
 			  "errorCode,Unsupported program");
 		return STATUS_SENT;
 	}
@@ -1005,7 +1006,8 @@ static enum sigma_cmd_result cmd_server_set_parameter(struct sigma_dut *dut,
 	}
 
 	prog = sigma_program_to_enum(var);
-	if (prog != PROGRAM_HS2_R2 && prog != PROGRAM_HS2_R3) {
+	if (prog != PROGRAM_HS2_R2 && prog != PROGRAM_HS2_R3 &&
+	    prog != PROGRAM_HS2_R4) {
 		send_resp(dut, conn, SIGMA_ERROR,
 			  "errorCode,Unsupported program");
 		return STATUS_SENT;

ap.c
@@ -8450,7 +8450,8 @@ write_conf:
 				dut->ap_radius_port);
 		fprintf(f, "auth_server_shared_secret=%s\n",
 			dut->ap_radius_password);
-		if (dut->program == PROGRAM_HS2_R3) {
+		if (dut->program == PROGRAM_HS2_R3 ||
+		    dut->program == PROGRAM_HS2_R4) {
 			fprintf(f, "radius_das_port=3799\n");
 			fprintf(f, "radius_das_client=0.0.0.0 %s\n",
 				dut->ap_radius_password);
@@ -9891,7 +9892,7 @@ static enum sigma_cmd_result cmd_ap_reset_default(struct sigma_dut *dut,
 	dut->ap_ocvc = dut->user_config_ap_ocvc;
 
 	if (dut->program == PROGRAM_HS2 || dut->program == PROGRAM_HS2_R2 ||
-	    dut->program == PROGRAM_HS2_R3 ||
+	    dut->program == PROGRAM_HS2_R3 || dut->program == PROGRAM_HS2_R4 ||
 	    dut->program == PROGRAM_IOTLP) {
 		int i;
 
@@ -9949,7 +9950,7 @@ static enum sigma_cmd_result cmd_ap_reset_default(struct sigma_dut *dut,
 	}
 
 	if (dut->program == PROGRAM_HS2_R2 || dut->program == PROGRAM_HS2_R3 ||
-	    dut->program == PROGRAM_IOTLP) {
+	     dut->program == PROGRAM_HS2_R4 || dut->program == PROGRAM_IOTLP) {
 		int i;
 		const char hessid[] = "50:6f:9a:00:11:22";
 

sigma_dut.h
@@ -893,6 +893,7 @@ struct sigma_dut {
 		PROGRAM_HE,
 		PROGRAM_HS2_R3,
 		PROGRAM_QM,
+		PROGRAM_HS2_R4,
 	} program;
 
 	enum device_type {

sta.c
@@ -5971,7 +5971,8 @@ cmd_sta_preset_testparameters(struct sigma_dut *dut, struct sigma_conn *conn,
 
 	val = get_param(cmd, "Program");
 	if (val && (strcasecmp(val, "HS2-R2") == 0 ||
-		    strcasecmp(val, "HS2-R3") == 0))
+		    strcasecmp(val, "HS2-R3") == 0 ||
+		    strcasecmp(val, "HS2-R4") == 0))
 		return cmd_sta_preset_testparameters_hs2_r2(dut, conn, intf,
 							    cmd);
 
@@ -9227,7 +9228,8 @@ static enum sigma_cmd_result cmd_sta_reset_default(struct sigma_dut *dut,
 	    lowi_cmd_sta_reset_default(dut, conn, cmd) < 0)
 		return ERROR_SEND_STATUS;
 
-	if (dut->program == PROGRAM_HS2_R2 || dut->program == PROGRAM_HS2_R3) {
+	if (dut->program == PROGRAM_HS2_R2 || dut->program == PROGRAM_HS2_R3 ||
+	    dut->program == PROGRAM_HS2_R4) {
 		unlink("SP/wi-fi.org/pps.xml");
 		if (system("rm -r SP/*") != 0) {
 		}
@@ -9330,13 +9332,14 @@ static enum sigma_cmd_result cmd_sta_reset_default(struct sigma_dut *dut,
 	set_ps(intf, dut, 0);
 
 	if (dut->program == PROGRAM_HS2 || dut->program == PROGRAM_HS2_R2 ||
-	    dut->program == PROGRAM_HS2_R3) {
+	    dut->program == PROGRAM_HS2_R3 || dut->program == PROGRAM_HS2_R4) {
 		wpa_command(intf, "SET interworking 1");
 		wpa_command(intf, "SET hs20 1");
 	}
 
 	if (dut->program == PROGRAM_HS2_R2 ||
 	    dut->program == PROGRAM_HS2_R3 ||
+	    dut->program == PROGRAM_HS2_R4 ||
 	    dut->program == PROGRAM_OCE) {
 		wpa_command(intf, "SET pmf 1");
 	} else {
@@ -13933,7 +13936,8 @@ enum sigma_cmd_result cmd_sta_send_frame(struct sigma_dut *dut,
 		return cmd_sta_send_frame_tdls(dut, conn, cmd);
 	if (val && (strcasecmp(val, "HS2") == 0 ||
 		    strcasecmp(val, "HS2-R2") == 0 ||
-		    strcasecmp(val, "HS2-R3") == 0))
+		    strcasecmp(val, "HS2-R3") == 0 ||
+		    strcasecmp(val, "HS2-R4") == 0))
 		return cmd_sta_send_frame_hs2(dut, conn, cmd);
 	if (val && strcasecmp(val, "VHT") == 0)
 		return cmd_sta_send_frame_vht(dut, conn, cmd);
@@ -14143,7 +14147,8 @@ int cmd_sta_set_parameter(struct sigma_dut *dut, struct sigma_conn *conn,
 	val = get_param(cmd, "program");
 	if (val && (strcasecmp(val, "HS2") == 0 ||
 		    strcasecmp(val, "HS2-R2") == 0 ||
-		    strcasecmp(val, "HS2-R3") == 0))
+		    strcasecmp(val, "HS2-R3") == 0 ||
+		    strcasecmp(val, "HS2-R4") == 0))
 		return cmd_sta_set_parameter_hs2(dut, conn, cmd, intf);
 
 	return -1;
@@ -16060,7 +16065,8 @@ static int sta_add_credential_sim(struct sigma_dut *dut,
 		return 0;
 	}
 
-	if (dut->program == PROGRAM_HS2_R2 || dut->program == PROGRAM_HS2_R3) {
+	if (dut->program == PROGRAM_HS2_R2 || dut->program == PROGRAM_HS2_R3 ||
+	    dut->program == PROGRAM_HS2_R4) {
 		/*
 		 * Set provisioning_sp for the test cases where SIM/USIM
 		 * provisioning is used.

utils.c
@@ -140,6 +140,8 @@ enum sigma_program sigma_program_to_enum(const char *prog)
 		return PROGRAM_HS2_R2;
 	if (strcasecmp(prog, "HS2-R3") == 0)
 		return PROGRAM_HS2_R3;
+	if (strcasecmp(prog, "HS2-R4") == 0)
+		return PROGRAM_HS2_R4;
 	if (strcasecmp(prog, "WFD") == 0)
 		return PROGRAM_WFD;
 	if (strcasecmp(prog, "DisplayR2") == 0)

commit 0b1be631760945a371f32f75e5f0946c3b33b3bd

server: Fix build
HS2-R4 addition had accidentally removed one line and that broke the
build with SERVER=y.

Fixes: 9a742ff95185 ("Add Program,HS2-R4 value")
Signed-off-by: Jouni Malinen <quic_jouni@quicinc.com>

server.c
@@ -880,6 +880,7 @@ static enum sigma_cmd_result cmd_server_request_status(struct sigma_dut *dut,
 	prog = sigma_program_to_enum(var);
 	if (prog != PROGRAM_HS2_R2 && prog != PROGRAM_HS2_R3 &&
 	    prog != PROGRAM_HS2_R4) {
+		send_resp(dut, conn, SIGMA_ERROR,
 			  "errorCode,Unsupported program");
 		return STATUS_SENT;
 	}