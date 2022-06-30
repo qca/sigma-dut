qca/sigma-dut

commit 25f27205b7926dc8e7f7f6ba32797b715397a26d

traffic: Allow pinging a broadcast address
Add support to ping a broadcast address.

Signed-off-by: Veerendranath Jakkam <quic_vjakkam@quicinc.com>

traffic.c
@@ -141,7 +141,7 @@ static enum sigma_cmd_result cmd_traffic_send_ping(struct sigma_dut *dut,
 	else
 		intf_arg[0] = '\0';
 	fprintf(f, "#!" SHELL "\n"
-		"ping%s -c %d%s -s %d%s -q%s %s > %s"
+		"ping%s -b -c %d%s -s %d%s -q%s %s > %s"
 		"/sigma_dut-ping.%d &\n"
 		"echo $! > %s/sigma_dut-ping-pid.%d\n",
 		type == 2 ? "6" : "", pkts, int_arg, size, extra,

commit c115721207081346afc78d56db69ce6441fd336d

traffic: Enable broadcast ping option only for IPv4 pings
The broadcast ping option is not applicable for ping6 (at least on
Android) even though the usage() has listed it as supported. Thus,
enabling -b option for ping6 may make the command fail. Fix this by
adding the -b argument only for the IPv4 case which actually needs this
for the broadcast ping.

Fixes: 25f27205b792 ("traffic: Allow pinging a broadcast address")
Signed-off-by: Veerendranath Jakkam <quic_vjakkam@quicinc.com>

traffic.c
@@ -141,10 +141,11 @@ static enum sigma_cmd_result cmd_traffic_send_ping(struct sigma_dut *dut,
 	else
 		intf_arg[0] = '\0';
 	fprintf(f, "#!" SHELL "\n"
-		"ping%s -b -c %d%s -s %d%s -q%s %s > %s"
+		"ping%s%s -c %d%s -s %d%s -q%s %s > %s"
 		"/sigma_dut-ping.%d &\n"
 		"echo $! > %s/sigma_dut-ping-pid.%d\n",
-		type == 2 ? "6" : "", pkts, int_arg, size, extra,
+		type == 2 ? "6" : "", type == 2 ? "" : " -b",
+		pkts, int_arg, size, extra,
 		intf_arg, dst, dut->sigma_tmpdir, id, dut->sigma_tmpdir, id);
 
 	fclose(f);