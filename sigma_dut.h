qca/sigma-dut

commit 25f27205b7926dc8e7f7f6ba32797b715397a26d

traffic: Allow pinging a broadcast address
Add support to ping a broadcast address.

Signed-off-by: Veerendranath Jakkam <quic_vjakkam@quicinc.com>

traffic.c
@@ -141,7 +141,7 @@ static enum sigma_cmd_result cmd_traffic_send_ping(struct sigma_dut *dut,
 	else
 		intf_arg[0] = '\0';
 	fprintf(f, "#!" SHELL "\n"
-		"ping%s -c %d%s -s %d%s -q%s %s > %s"
+		"ping%s -b -c %d%s -s %d%s -q%s %s > %s"
 		"/sigma_dut-ping.%d &\n"
 		"echo $! > %s/sigma_dut-ping-pid.%d\n",
 		type == 2 ? "6" : "", pkts, int_arg, size, extra,

commit 021617228197457daf7b4109b6bd4b816036712e

traffic: Include -b argument for ping only for 255.255.255.255
By default enabling broadcast pinging causes issues with tests when the
framesize is more than 1472 due to max MTU size limit. This happens with
the -b argument even if the actual IPv4 address is a unicast address. To
avoid such issues enable broadcast pinging with -b only when the IPv4
address is 255.255.255.255.

Fixes: 25f27205b792 ("traffic: Allow pinging a broadcast address")
Signed-off-by: Shivani Baranwal <quic_shivbara@quicinc.com>

sigma_dut.h
@@ -94,6 +94,7 @@ struct sigma_dut;
 #define MAX_RADIO 3
 
 #define NAN_AWARE_IFACE "wifi-aware0"
+#define BROADCAST_ADDR "255.255.255.255"
 
 /* Set default operating channel width 80 MHz */
 #define VHT_DEFAULT_OPER_CHWIDTH AP_80_VHT_OPER_CHWIDTH

traffic.c
@@ -41,6 +41,7 @@ static enum sigma_cmd_result cmd_traffic_send_ping(struct sigma_dut *dut,
 	int dscp = 0, use_dscp = 0;
 	char extra[100], int_arg[100], intf_arg[100], ip_dst[100], ping[100];
 	struct in6_addr ip6_addr;
+	bool broadcast = false;
 
 	val = get_param(cmd, "Type");
 	if (!val)
@@ -78,6 +79,14 @@ static enum sigma_cmd_result cmd_traffic_send_ping(struct sigma_dut *dut,
 	if (val == NULL)
 		return INVALID_SEND_STATUS;
 	size = atoi(val);
+	if (type != 2 && strcmp(dst, BROADCAST_ADDR) == 0) {
+		if (size > 1472) {
+			send_resp(dut, conn, SIGMA_ERROR,
+				  "ErrorCode,Unsupported broadcast ping frame size");
+			return STATUS_SENT;
+		}
+		broadcast = true;
+	}
 
 	val = get_param(cmd, "frameRate");
 	if (val == NULL)
@@ -144,7 +153,7 @@ static enum sigma_cmd_result cmd_traffic_send_ping(struct sigma_dut *dut,
 		"ping%s%s -c %d%s -s %d%s -q%s %s > %s"
 		"/sigma_dut-ping.%d &\n"
 		"echo $! > %s/sigma_dut-ping-pid.%d\n",
-		type == 2 ? "6" : "", type == 2 ? "" : " -b",
+		type == 2 ? "6" : "", broadcast ? " -b" : "",
 		pkts, int_arg, size, extra,
 		intf_arg, dst, dut->sigma_tmpdir, id, dut->sigma_tmpdir, id);
 