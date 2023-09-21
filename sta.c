qca/sigma-dut

commit 3296dcedfd6fe449809e54734cfb4a917257f0bc

EHT: Add support to configure 1024 and 512 BA buffer size
Extend the set of recognized values for ADDBAReq_BufSize and
ADDBAResp_BufSize parameters to include gt512, gt256, eq512, and eq256
to support configuration of 1024 and 512 BA buffer size.

Signed-off-by: Jouni Malinen <quic_jouni@quicinc.com>

sta.c
@@ -12225,7 +12225,13 @@ cmd_sta_set_wireless_vht(struct sigma_dut *dut, struct sigma_conn *conn,
 	if (val) {
 		int buf_size;
 
-		if (strcasecmp(val, "gt64") == 0)
+		if (strcasecmp(val, "gt512") == 0)
+			buf_size = 1024;
+		else if (strcasecmp(val, "gt256") == 0 ||
+			 strcasecmp(val, "eq512"))
+			buf_size = 512;
+		else if (strcasecmp(val, "gt64") == 0 ||
+			 strcasecmp(val, "eq256"))
 			buf_size = 256;
 		else
 			buf_size = 64;
@@ -12241,7 +12247,13 @@ cmd_sta_set_wireless_vht(struct sigma_dut *dut, struct sigma_conn *conn,
 	if (val) {
 		int buf_size;
 
-		if (strcasecmp(val, "gt64") == 0)
+		if (strcasecmp(val, "gt512") == 0)
+			buf_size = 1024;
+		else if (strcasecmp(val, "gt256") == 0 ||
+			 strcasecmp(val, "eq512"))
+			buf_size = 512;
+		else if (strcasecmp(val, "gt64") == 0 ||
+			 strcasecmp(val, "eq256"))
 			buf_size = 256;
 		else
 			buf_size = 64;

commit 99143d3e73f49eec5e30b857b5fb2f9b7b79b2df

EHT: Fix the BA buffer size parameter check for the new values
Fix the buffer size param check condition to use the strcasecmp() return
value correctly.

Fixes: 3296dcedfd6f ("EHT: Add support to configure 1024 and 512 BA buffer size")
Signed-off-by: Jouni Malinen <quic_jouni@quicinc.com>

sta.c
@@ -12281,10 +12281,10 @@ cmd_sta_set_wireless_vht(struct sigma_dut *dut, struct sigma_conn *conn,
 		if (strcasecmp(val, "gt512") == 0)
 			buf_size = 1024;
 		else if (strcasecmp(val, "gt256") == 0 ||
-			 strcasecmp(val, "eq512"))
+			 strcasecmp(val, "eq512") == 0)
 			buf_size = 512;
 		else if (strcasecmp(val, "gt64") == 0 ||
-			 strcasecmp(val, "eq256"))
+			 strcasecmp(val, "eq256") == 0)
 			buf_size = 256;
 		else
 			buf_size = 64;
@@ -12303,10 +12303,10 @@ cmd_sta_set_wireless_vht(struct sigma_dut *dut, struct sigma_conn *conn,
 		if (strcasecmp(val, "gt512") == 0)
 			buf_size = 1024;
 		else if (strcasecmp(val, "gt256") == 0 ||
-			 strcasecmp(val, "eq512"))
+			 strcasecmp(val, "eq512") == 0)
 			buf_size = 512;
 		else if (strcasecmp(val, "gt64") == 0 ||
-			 strcasecmp(val, "eq256"))
+			 strcasecmp(val, "eq256") == 0)
 			buf_size = 256;
 		else
 			buf_size = 64;